4093329261068af7a13a2b107548a47bc8fff765
nosuzuki <nosuzuki@postcard.st>
2019-12-14 21:40:51 +0900

    Fixed to make sure EFI partitions are mounted while updating kernel.

--- install-update-service.sh
+++ install-update-service.sh
@@ -7,6 +7,22 @@
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
 cp $SCRIPT_DIR/update-refind.sh /boot
-cp $SCRIPT_DIR/update-refind.service /etc/systemd/system
+
+efis=$(ls -d /boot/efi_* | xargs)
+cat << EOF > /etc/systemd/system/update-refind.service 
+[Unit]
+Description=Copy latest kernel to EFI patitions and update refind.conf
+RequiresMountsFor=$efis
+Before=shutdown.target reboot.target poweroff.target halt.target
+DefaultDependencies=no
+
+[Service]
+Type=oneshot
+ExecStart=/boot/update-refind.sh
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
 systemctl enable update-refind
 systemctl start update-refind
