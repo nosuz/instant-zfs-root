0672fe04c85f40724c6d97611be72a908ae030ec
nosuzuki <nosuzuki@postcard.st>
2019-12-25 09:30:27 +0900

    cd to /boot

--- update-refind.sh
+++ update-refind.sh
@@ -5,10 +5,12 @@
 
 exec 1>> /boot/update-refind.log 2>&1
 
+cd /boot
+
 echo ----------------
 date
 
-kernel_path=$(ls /boot/vmlinuz-* | sort -V | tail -n 1)
+kernel_path=$(ls vmlinuz-* | sort -V | tail -n 1)
 kernel=$(basename $kernel_path)
 echo $kernel
 src=$(stat -c %Y $kernel_path)
@@ -20,28 +22,28 @@ for efi in $(ls /boot | grep efi_); do
     echo $efi
 
     # make sure EFI partition is mounted
-    [[ -e /boot/$efi/efi ]] || break
+    [[ -e $efi/efi ]] || break
 
     update=false
-    dst=$(stat -c %Y /boot/$efi/$kernel || echo 0)
+    dst=$(stat -c %Y $efi/$kernel || echo 0)
     if (( $src > $dst )); then
-	cp /boot/$kernel /boot/$efi
+	cp $kernel $efi
 	update=true
     fi
 
-    dst=$(stat -c %Y /boot/$efi/$initrd || echo 0)
+    dst=$(stat -c %Y $efi/$initrd || echo 0)
     if (( $src > $dst )); then
-	cp /boot/$initrd /boot/$efi
+	cp $initrd $efi
 	update=true
     fi
 
     if $update; then
 	echo "Update refind.conf"
-	if [[ -f /boot/$efi/efi/boot/refind.conf ]]; then
-	    mv /boot/$efi/efi/boot/refind.conf /boot/$efi/efi/boot/refind.conf.bak
+	if [[ -f $efi/efi/boot/refind.conf ]]; then
+	    mv $efi/efi/boot/refind.conf $efi/efi/boot/refind.conf.bak
 	fi
 
-	cat << EOF > /boot/$efi/efi/boot/refind.conf
+	cat << EOF > $efi/efi/boot/refind.conf
 timeout 5
 icons_dir EFI/boot/icons/
 scanfor manual
