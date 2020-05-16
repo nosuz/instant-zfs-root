bf7c4d82a8f79e32c52437096920853648f21b71
nosuzuki <nosuzuki@postcard.st>
2020-05-16 21:24:17 +0900

    Redirect log into logfile from the trim cron job.

--- trim-zfs-pools.sh
+++ trim-zfs-pools.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 export PATH=$PATH:/usr/sbin:/sbin
 
+# redirect STDOUT and STDERR to logfile
+exec &>> /var/log/trim-zfs-pool.log
+
 for pool in $(zpool list -H -o health,name | awk '{if ($1 == "ONLINE") print $2}'); do
     zpool trim $pool
 done
