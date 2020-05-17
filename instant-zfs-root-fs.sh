bf64db12cadc69a7fda0e6c0cb4f7f36405a951d
nosuzuki <nosuzuki@postcard.st>
2020-05-17 22:03:39 +0900

    Fixed NVMe has no ID_BUS entry.

--- instant-zfs-root-fs.sh
+++ instant-zfs-root-fs.sh
@@ -499,7 +499,8 @@ if [[ ! -e /etc/udev/rules.d/91-zfs-vdev.rules ]] ; then
 # https://github.com/zfsonlinux/pkg-zfs/wiki/HOWTO-install-Ubuntu-14.04-or-Later-to-a-Native-ZFS-Root-Filesystem
 
 # Create a by-id style link in /dev for zfs_member vdev. Needed by boot
-KERNEL=="sd*[0-9]|nvme[0-9]n[0-9]p[0-9]", ENV{ID_FS_TYPE}=="zfs_member", SYMLINK+="disk/zfs/\$env{ID_BUS}-\$env{ID_SERIAL}-part%n"
+KERNEL=="sd*[0-9]", ENV{ID_FS_TYPE}=="zfs_member", SYMLINK+="disk/zfs/\$env{ID_BUS}-\$env{ID_SERIAL}-part%n"
+KERNEL=="nvme[0-9]n[0-9]p[0-9]", ENV{ID_FS_TYPE}=="zfs_member", SYMLINK+="disk/zfs/nvme-\$env{ID_SERIAL}-part%n"
 EOF_UDEV
 fi
 
