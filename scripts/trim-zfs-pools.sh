748f39b45986288c6c406a68ebc73a42cf787bf1
nosuzuki <nosuzuki@postcard.st>
2020-09-03 19:28:41 +0900

    Log the date when ZFS trim command started as cron job.

--- scripts/trim-zfs-pools.sh
+++ scripts/trim-zfs-pools.sh
@@ -4,6 +4,10 @@ export PATH=$PATH:/usr/sbin:/sbin
 # redirect STDOUT and STDERR to logfile
 exec &>> /var/log/trim-zfs-pool.log
 
+echo ----------------
+date
+
 for pool in $(zpool list -H -o health,name | awk '{if ($1 == "ONLINE") print $2}'); do
+    echo trim $pool
     zpool trim $pool
 done
